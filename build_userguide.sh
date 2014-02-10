ab6e02177e474144375be7ed4fec33ccce4ce068
Jason Parham <bluemellophone@gmail.com>
2014-02-10 14:25:10 -0500

    Added mac standalone gatekeeper instructions

--- build_userguide.sh
+++ build_userguide.sh
@@ -6,3 +6,11 @@ rm *.out
 rm HotSpotterUserGuide.pdf
 mv HotSpotterUserGuide-compress.pdf HotSpotterUserGuide.pdf
 cp HotSpotterUserGuide.pdf ~/code/hotspotter/_doc/HotSpotterUserGuide.pdf
+
+pdflatex HotSpotterUserGuideMac.tex
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=HotSpotterUserGuideMac-compress.pdf HotSpotterUserGuideMac.pdf
+rm *.aux
+rm *.log
+rm *.out
+rm HotSpotterUserGuideMac.pdf
+mv HotSpotterUserGuideMac-compress.pdf HotSpotterUserGuideMac.pdf
